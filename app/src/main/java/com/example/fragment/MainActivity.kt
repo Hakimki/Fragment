f2386a001be26743fbdb212a38b0d554b5b3507b
Hakimki <fahmihhakim8@gmail.com>
2023-03-16 11:30:03 +0700

    Fragment

--- app/src/main/java/com/example/fragment/MainActivity.kt
+++ app/src/main/java/com/example/fragment/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val view_pager = findViewById<ViewPager>(R.id.view_pager)
+        val tabs = findViewById<TabLayout>(R.id.tabs)
 
         view_pager.adapter = ViewPagerAdapter(this, supportFragmentManager)
         tabs.setupWithViewPager(view_pager)
